19e7eb2c9c2688043039d843359db6486dc374ce
longro3000 <longro3000@gmail.com>
2018-12-07 03:40:08 +0200

    first commit

---
+++ Client/src/components/HomePage/home_index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class home_index extends Component {
+  render() {
+    return (
+      <div>React simple starter</div>
+    );
+  }
+}
+
+export default home_index;
---
+++ Client/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
+
+import reducers from './reducers';
+import HomeIndex from './components/HomePage/home_index';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+        <Switch>
+          <Route path='/' component={HomeIndex}/>
+        </Switch>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('#root'));
