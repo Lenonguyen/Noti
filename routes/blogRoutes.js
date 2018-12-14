2c83e16af40f9b19376b83222314c4d200fe284f
lenonguyen@gmail.com <lenonguyen@gmail.com>
2018-12-14 17:00:20 +0200

    Add a route for posting blog

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,3 +1,26 @@
+const mongoose = require('mongoose');
+const requireLogin = require('../middlewares/requireLogin');
+
+const Blog = mongoose.model('Blog');
+
 module.exports = app => {
-     app.get('/')
-}
+     app.get('/api/blogs');
+     
+     //Post a blog
+     app.post('/api/blogs', requireLogin, async (req,res) => {
+          const {title, content} = req.body;
+
+          const blog = new Blog({
+               title,
+               content,
+               _user: req.user.id
+          });
+
+          try {
+               await blog.save();
+               res.send(blog);
+          } catch(err) {
+               res.send(400, err);
+          }
+     });
+};
