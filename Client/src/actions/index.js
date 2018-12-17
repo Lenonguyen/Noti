e6f267c834a8782895fa0ef093c7968ec0dc204d
longro3000 <longro3000@gmail.com>
2018-12-17 10:00:53 +0200

    Header get user info and search bar

--- Client/src/actions/index.js
+++ Client/src/actions/index.js
@@ -1,10 +1,19 @@
 import axios from 'axios';
 
 export const GOOGLE_AUTH = 'google_auth';
+export const SEARCH_NOTI = 'google_auth';
 
 export async function googleAuth() {
      const res = await axios.get('/api/current_user');
      console.log(res.data);
      return {
-       type: GOOGLE_AUTH, payload: res.data };
+       type: GOOGLE_AUTH, payload: res.data
+     };
+};
+
+export const searchNoti(values) {
+    const data = values;
+    return {
+      type: SEARCH_NOTI, payload: data
+    };
 };
