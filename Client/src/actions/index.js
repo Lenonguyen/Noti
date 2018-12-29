eb8a017af08911b5e86a4661616f657b26e69354
lenonguyen@gmail.com <lenonguyen@gmail.com>
2018-12-29 22:37:07 +0200

    Merge branch 'master' into leno

--- Client/src/actions/index.js
+++ Client/src/actions/index.js
@@ -4,7 +4,6 @@ export const GOOGLE_AUTH = 'google_auth';
 
 export async function googleAuth() {
      const res = await axios.get('/api/current_user');
-     console.log(res.data);
      return {
        type: GOOGLE_AUTH, payload: res.data };
 };
